60769f90b6758cefdd5dc635d65a66be0daca4f4
Vladimir Polyakov <nrd11k@gmail.com>
2018-10-10 11:32:51 +0300

    wip

--- build.js
+++ build.js
@@ -4,14 +4,11 @@ const layouts = require('metalsmith-layouts');
 const markdown = require('metalsmith-markdown');
 const sass = require('metalsmith-sass');
 const watch = require('metalsmith-watch');
+const autoprefixer = require('metalsmith-autoprefixer');
 
 const metalsmith = Metalsmith(__dirname)
 	.source('src')
 	.destination('build')
-	.use(sass({
-		sourceMap: true,
-		sourceMapContents: true
-	}))
 	.use(markdown())
 	.use(layouts({
 		engineOptions: {
@@ -22,12 +19,22 @@ const metalsmith = Metalsmith(__dirname)
 if (process.env.NODE_ENV === 'development') {
 	metalsmith
 		.use(debug())
+		.use(sass({
+			sourceMap: true,
+			sourceMapContents: true
+		}))
 		.use(watch({
 			paths: {
 				'src/**/*': '**/*',
 				'layouts/**/*': '**/*'
 			},
 		}));
+} else {
+	metalsmith
+		.use(sass({
+			outputStyle: 'nested'
+		}))
+		.use(autoprefixer());
 }
 
 metalsmith.build(function(err) {
