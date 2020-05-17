e8b203bbff72ff4fe8b0dd61b377f30fd55d1b37
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-05-17 18:18:58 +0300

    remove useless folder

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link, graphql} from 'gatsby';
-import {Layout} from '../../components';
+import {Layout} from '../components';
 
 function Blog(props) {
 	const {edges} = props.data.allMarkdownRemark;
