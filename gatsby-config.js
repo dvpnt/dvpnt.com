ccea7ad28297b51a0f74b2f5e4a304cab2e068d6
Mikhail Vyrodov <me.vyrodov@ya.ru>
2020-06-07 19:11:41 +0300

    add image plugins, iframe plugin, and some improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,11 @@ module.exports = {
 		siteUrl: process.env.NODE_ENV === 'production' ? 'https://dvpnt.com' : 'http://localhost:8000'
 	},
 	plugins: [
+		'gatsby-plugin-catch-links',
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sass',
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
 		{
 			resolve: 'gatsby-source-filesystem',
 			options: {
@@ -19,8 +24,16 @@ module.exports = {
 			resolve: 'gatsby-transformer-remark',
 			options: {
 				plugins: [
+					'gatsby-remark-copy-linked-files',
 					'gatsby-remark-smartypants',
 					'gatsby-remark-autolink-headers',
+					'gatsby-remark-responsive-iframe',
+					{
+						resolve: 'gatsby-remark-images',
+						options: {
+							maxWidth: 800
+						}
+					},
 					{
 						resolve: 'gatsby-remark-prismjs',
 						options: {
@@ -45,8 +58,17 @@ module.exports = {
 				pagesPaths: ['content/posts']
 			}
 		},
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-sass',
-		'gatsby-plugin-catch-links'
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				name: 'dvpnt',
+				short_name: 'dvpnt',
+				start_url: '/blog',
+				background_color: '#ffffff',
+				theme_color: '#06038d',
+				// icon: 'src/assets/icon.png',
+				display: 'minimal-ui'
+			}
+		}
 	]
 };
